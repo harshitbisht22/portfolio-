a96383f1def8bdda48c513694faaddf6114b44f6
Harshit Bisht <Harshit.bisht.22@gmail.com>
2023-03-03 22:55:09 +0530

    n

--- components/Intro.js
+++ components/Intro.js
@@ -3,23 +3,14 @@ import { useEffect, useState } from "react"
 export default function Intro(params) {
 
     const[classes,setClasses]=useState('');
- 
-    useEffect( () =>{
-
-        setClasses('visible');
-
-    },[] );
-
-
-
-    return(
+ return(
         <section id= "intro" className={classes}>
     <div>
       <p>
         I am <span>Harshit Bisht</span> <br/> 
         Web developer & Part Time Designer
       </p>
-      <a href="">Contact me </a>
+      <a href="#">Contact me </a>
     </div>
     <div>
       <img src="/img/har.png" alt="" />
